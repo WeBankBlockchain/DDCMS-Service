d58a5634213e32f04a0c43b39739f3a8cc982682
arc0035 <aoronchu@gmail.com>
2023-02-17 16:16:57 +0800

    .

--- src/main/java/com/webank/data/brain/model/account/AccountID.java
+++ src/main/java/com/webank/data/brain/model/account/AccountID.java
@@ -2,4 +2,10 @@ package com.webank.data.brain.model.account;
 
 public class AccountID {
     private byte[] content;
+
+    @Override
+    public String toString(){
+        return null;
+    }
+
 }
