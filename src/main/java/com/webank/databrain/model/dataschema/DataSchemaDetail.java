710b264ccc95570dbcc42b0443859b09f0cbbdb9
wangyue <wangyue@wangyuedeMac-mini.local>
2023-02-24 11:35:38 +0800

    schema

--- src/main/java/com/webank/databrain/model/dataschema/DataSchemaDetail.java
+++ src/main/java/com/webank/databrain/model/dataschema/DataSchemaDetail.java
@@ -1,4 +1,32 @@
 package com.webank.databrain.model.dataschema;
 
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
 public class DataSchemaDetail {
+
+    private Long schemaId;
+
+    private Long providerId;
+
+    private Long productId;
+
+    private Integer version;
+
+    private Integer visible;
+
+    private String description;
+
+    private String usage;
+
+    private Integer price;
+
+    private Long visitId;
+
+    private LocalDateTime createTime;
+
+    private LocalDateTime updateTime;
 }
--- src/main/java/com/webank/databrain/service/DataSchemaService.java
+++ src/main/java/com/webank/databrain/service/DataSchemaService.java
@@ -1,38 +1,117 @@
 package com.webank.databrain.service;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.core.toolkit.Wrappers;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.webank.databrain.db.dao.ISchemaService;
+import com.webank.databrain.db.entity.DataSchemaDataObject;
+import com.webank.databrain.db.entity.ProductDataObject;
+import com.webank.databrain.db.entity.UserInfo;
 import com.webank.databrain.model.common.Paging;
 import com.webank.databrain.model.common.PagingResult;
 import com.webank.databrain.model.dataschema.DataSchemaDetail;
 import com.webank.databrain.model.dataschema.DataSchemaSummary;
 import com.webank.databrain.model.dataschema.NewDataSchema;
 import com.webank.databrain.model.dataschema.UpdatedDataSchema;
+import com.webank.databrain.model.product.ProductDetail;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
+@Service
 public class DataSchemaService {
+
+    @Autowired
+    private ISchemaService schemaService;
 
     public List<PagingResult<DataSchemaSummary>> listDataSchemas(Paging paging, boolean forAudit) {
         return null;
     }
 
-    public List<PagingResult<DataSchemaSummary>> listDataSchemasByEnterprise(String ownerId, Paging paging) {
-        return null;
+    public PagingResult<DataSchemaDetail> listDataSchemasByEnterprise(String providerId, Paging paging) {
+        IPage<DataSchemaDataObject> result = schemaService.page(new Page<>(paging.getPageNo(),paging.getPageSize()),
+                Wrappers.<DataSchemaDataObject>query().eq("providerId",providerId));
+        List<DataSchemaDataObject> dataSchemaDataObjects = result.getRecords();
+        List<DataSchemaDetail> dataSchemaDetails = new ArrayList<>();
+
+        dataSchemaDataObjects.forEach(dataSchemaDataObject -> {
+            DataSchemaDetail dataSchemaDetail = new DataSchemaDetail();
+            BeanUtils.copyProperties(dataSchemaDataObject,dataSchemaDetail);
+            dataSchemaDetails.add(dataSchemaDetail);
+        });
+        return new PagingResult<>(
+                dataSchemaDetails,
+                result.getCurrent(),
+                result.getSize(),
+                result.getTotal(),
+                result.getPages());
     }
 
-    public List<PagingResult<DataSchemaSummary>> listDataSchemasByProduct(String productId, Paging paging) {
-        return null;
+    public PagingResult<DataSchemaDetail> listDataSchemasByProduct(String productId, Paging paging) {
+        IPage<DataSchemaDataObject> result = schemaService.page(new Page<>(paging.getPageNo(),paging.getPageSize()),
+                Wrappers.<DataSchemaDataObject>query().eq("productId",productId));
+        List<DataSchemaDataObject> dataSchemaDataObjects = result.getRecords();
+        List<DataSchemaDetail> dataSchemaDetails = new ArrayList<>();
+
+        dataSchemaDataObjects.forEach(dataSchemaDataObject -> {
+            DataSchemaDetail dataSchemaDetail = new DataSchemaDetail();
+            BeanUtils.copyProperties(dataSchemaDataObject,dataSchemaDetail);
+            dataSchemaDetails.add(dataSchemaDetail);
+        });
+        return new PagingResult<>(
+                dataSchemaDetails,
+                result.getCurrent(),
+                result.getSize(),
+                result.getTotal(),
+                result.getPages());
     }
 
-    public List<PagingResult<DataSchemaSummary>> listDataSchemasByTag(String tagId, Paging paging) {
-        return null;
+    public PagingResult<DataSchemaDetail> listDataSchemasByTag(String tagId, Paging paging) {
+        IPage<DataSchemaDataObject> result = schemaService.page(new Page<>(paging.getPageNo(),paging.getPageSize()),
+                Wrappers.<DataSchemaDataObject>query().eq("tagId",tagId));
+        List<DataSchemaDataObject> dataSchemaDataObjects = result.getRecords();
+        List<DataSchemaDetail> dataSchemaDetails = new ArrayList<>();
+
+        dataSchemaDataObjects.forEach(dataSchemaDataObject -> {
+            DataSchemaDetail dataSchemaDetail = new DataSchemaDetail();
+            BeanUtils.copyProperties(dataSchemaDataObject,dataSchemaDetail);
+            dataSchemaDetails.add(dataSchemaDetail);
+        });
+        return new PagingResult<>(
+                dataSchemaDetails,
+                result.getCurrent(),
+                result.getSize(),
+                result.getTotal(),
+                result.getPages());
     }
 
-    public List<PagingResult<DataSchemaSummary>> listDataSchemasBySearch(String keyword, Paging paging) {
-        return null;
+    public PagingResult<DataSchemaDetail> listDataSchemasBySearch(String keyword, Paging paging) {
+        IPage<DataSchemaDataObject> result = schemaService.page(new Page<>(paging.getPageNo(),paging.getPageSize()),
+                Wrappers.<DataSchemaDataObject>query().like("description",keyword));
+        List<DataSchemaDataObject> dataSchemaDataObjects = result.getRecords();
+        List<DataSchemaDetail> dataSchemaDetails = new ArrayList<>();
+
+        dataSchemaDataObjects.forEach(dataSchemaDataObject -> {
+            DataSchemaDetail dataSchemaDetail = new DataSchemaDetail();
+            BeanUtils.copyProperties(dataSchemaDataObject,dataSchemaDetail);
+            dataSchemaDetails.add(dataSchemaDetail);
+        });
+        return new PagingResult<>(
+                dataSchemaDetails,
+                result.getCurrent(),
+                result.getSize(),
+                result.getTotal(),
+                result.getPages());
     }
 
-    public DataSchemaDetail getDataSchemaById(String id){
-        return null;
+    public DataSchemaDetail getDataSchemaById(long schemaId){
+        DataSchemaDataObject schemaDataObject = schemaService.getOne(Wrappers.<DataSchemaDataObject>query().eq("schemaId",schemaId));
+        DataSchemaDetail schemaDetail = new DataSchemaDetail();
+        BeanUtils.copyProperties(schemaDataObject,schemaDetail);
+        return schemaDetail;
     }
 
     public String createDataSchema(NewDataSchema dataSchema, byte[] signature){
