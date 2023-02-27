745b12f1e209e742fa55853cf05e258044b01bfa
wangyue <wangyue@wangyuedeMac-mini.local>
2023-02-27 15:58:43 +0800

    cors

--- src/main/java/com/webank/databrain/controller/ProductController.java
+++ src/main/java/com/webank/databrain/controller/ProductController.java
@@ -9,16 +9,14 @@ import com.webank.databrain.model.product.ProductIdName;
 import com.webank.databrain.service.ProductService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @Slf4j
 @RequestMapping("product")
+@CrossOrigin(origins = "*")
 public class ProductController {
 
     @Autowired
