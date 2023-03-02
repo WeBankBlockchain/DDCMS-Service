09113de8a2148431b12cd58b54c35f6a346f23d5
dalaocu <njumjy06@126.com>
2023-03-02 18:22:09 +0800

    userinfo rm location

--- src/main/java/com/webank/databrain/model/account/NormalUserDetail.java
+++ src/main/java/com/webank/databrain/model/account/NormalUserDetail.java
@@ -2,15 +2,13 @@ package com.webank.databrain.model.account;
 
 import lombok.Data;
 
-//{\"name\":\"aaa\",\"contact\":\"bbb\",\"location\":\"ccc\",\"email\":\"ddd\",\"certType\":\"eee\",\"certNum\":\"fff\"}
+//{\"name\":\"aaa\",\"contact\":\"bbb\",\"email\":\"ddd\",\"certType\":\"eee\",\"certNum\":\"fff\"}
 @Data
 public class NormalUserDetail {
 
     private String name;
 
     private String contact;
-
-    private String location;
 
     private String email;
 
