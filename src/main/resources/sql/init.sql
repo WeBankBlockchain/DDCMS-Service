e1f2ff0b8b56d7bafdc815562232b42536b74561
arc0035 <aoronchu@gmail.com>
2023-03-06 20:05:53 +0800

    .

--- src/main/resources/sql/init.sql
+++ src/main/resources/sql/init.sql
@@ -2,7 +2,7 @@
  CREATE TABLE `t_account_info` (
         `pk_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
         `username` varchar(255) NOT NULL DEFAULT '' COMMENT '账号',
-        `did` varchar(255) NOT NULL DEFAULT '' COMMENT 'DID',
+        `did` varchar(255) NOT NULL DEFAULT '' COMMENT 'DID',--这个不宜直接设为主键，chatgpt建议还是用自增id作为主键，因为简单小巧灵活--
         `account_type` int(4) NOT NULL DEFAULT 0 COMMENT '用户类型',
         `private_key` varchar(66) NOT NULL DEFAULT '' COMMENT '私钥',
         `salt` varchar(255) NOT NULL,
@@ -19,7 +19,7 @@
 -- 个人用户表 --
  CREATE TABLE `t_person_info` (
    `pk_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-   `person_id` varchar(255) NOT NULL DEFAULT '' COMMENT 'DID',
+   `person_id` bigint(20) unsigned NOT NULL COMMENT '账户ID',--chatgpt建议外键采用pkid，因为和具体的did字段解藕了--
    `person_name` varchar(32) NOT NULL DEFAULT '' COMMENT '姓名',
    `person_contact` varchar(32) NOT NULL DEFAULT '' COMMENT '个人联系方式',
    `person_email` varchar(128) NOT NULL DEFAULT '' COMMENT '个人邮箱',
@@ -34,7 +34,7 @@
 -- 机构表 --
  CREATE TABLE `t_company_info` (
    `pk_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-   `company_id` varchar(255) NOT NULL DEFAULT '' COMMENT 'DID',
+   `company_id` bigint(20) unsigned NOT NULL COMMENT '账户ID',--chatgpt建议外键采用pkid，因为和具体的did字段解藕了--
    `company_name` varchar(255) NOT NULL COMMENT '公司名称',
    `company_cert_type` int(4) NOT NULL DEFAULT 0 comment '法人证件类型',
    `company_cert_content` text COMMENT '公司证件内容',
@@ -50,9 +50,9 @@
 -- 产品表 --
  CREATE TABLE `t_product_info` (
    `pk_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-   `product_id` varchar(255) NOT NULL DEFAULT '' COMMENT '产品id',
+   `product_gid` varchar(255) NOT NULL DEFAULT '' COMMENT '产品id',
    `product_name` varchar(255) NOT NULL DEFAULT '' COMMENT '产品名称',
-   `provider_id` varchar(255) NOT NULL DEFAULT '' COMMENT '提供方id',
+   `provider_id` bigint(20) unsigned NOT NULL COMMENT '提供方ID',--chatgpt建议外键采用pkid，因为和具体的did字段解藕了--
    `product_desc` text COMMENT '产品详情',
    `status` int(4) NOT NULL DEFAULT 0 COMMENT '审核状态',
    `review_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '审核时间',
@@ -66,7 +66,6 @@
    `pk_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
    `tag_name` varchar(64) NOT NULL DEFAULT '' COMMENT '标签名',
    `heat` int(32) NOT NULL DEFAULT 0 COMMENT '热度',
-   `schema_id_list` varchar(255) NOT NULL DEFAULT '' COMMENT 'schemaId列表',
    `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
    `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
    PRIMARY KEY (`pk_id`),
@@ -76,11 +75,11 @@
 -- 数据目录表 --
  CREATE TABLE `t_data_schema_info` (
    `pk_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-   `data_schema_id` varchar(255) NOT NULL DEFAULT '' COMMENT '数据目录id',
+   `data_schema_gid` varchar(255) NOT NULL DEFAULT '' COMMENT '数据目录全局id',
    `data_schema_name` varchar(255) NOT NULL DEFAULT '' COMMENT '数据目录名称',
    `tag_id` varchar(64) NOT NULL DEFAULT '' COMMENT '标签',
    `provider_id` varchar(255) NOT NULL DEFAULT 0 COMMENT '提供方id',
-   `product_id` varchar(255) NOT NULL DEFAULT '' COMMENT '产品id',
+   `product_id` bigint(20) unsigned NOT NULL COMMENT '产品ID',--chatgpt建议外键采用pkid，因为和具体的did字段解藕了--
    `version` int(4) NOT NULL DEFAULT 0 COMMENT '版本号',
    `visible` int(4) NOT NULL DEFAULT 0 COMMENT '是否可见',
    `data_schema_desc` text COMMENT '描述',
@@ -95,7 +94,7 @@
 -- 数据目录访问详情--
  CREATE TABLE `t_data_schema_access_info` (
     `pk_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
-    `data_schema_id` varchar(255) NOT NULL DEFAULT '' COMMENT '目录id',
+    `data_schema_id` bigint(20) unsigned NOT NULL COMMENT '数据目录主键',--chatgpt建议外键采用pkid，因为和具体的did字段解藕了--
     `data_format` int(4) NOT NULL DEFAULT 0 COMMENT '类型，json-0，xml-1，doc-2，pic-3...',
     `data_protocol` int(4) NOT NULL DEFAULT 0 COMMENT '类型，HTTP-0，HTTPS-1，SFTP-2...',
 --    `schema` text  COMMENT '数据schema,格式为Json',
@@ -112,11 +111,11 @@
   CREATE TABLE `t_session_info` (
     `pk_id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
     `token` varchar(255) NOT NULL DEFAULT '' COMMENT 'token',
-    `did`  varchar(255) NOT NULL DEFAULT '' COMMENT 'did',
+    `account_id` bigint(20) unsigned NOT NULL COMMENT '账户ID',--chatgpt建议外键采用pkid，因为和具体的did字段解藕了--
     `expiredAt` timestamp NOT NULL COMMENT '过期时间',
     `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
     `update_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
     PRIMARY KEY (`pk_id`),
     UNIQUE KEY (`token`),
-    UNIQUE KEY (`did`)
+    UNIQUE KEY (`account_id`)
  ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
