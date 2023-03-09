da5f65b16b5b1ea90993666db3d15cd2c4ce974e
arc0035 <aoronchu@gmail.com>
2023-03-09 15:58:59 +0800

    .

---
+++ src/main/java/com/webank/databrain/model/resp/tags/CreateTagResponse.java
@@ -0,0 +1,11 @@
+package com.webank.databrain.model.resp.tags;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class CreateTagResponse {
+
+    public long id;
+}
