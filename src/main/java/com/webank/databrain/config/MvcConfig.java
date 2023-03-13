04a96ca84d484ea63132eb1cdaae9f23a7e0072a
arc0035 <aoronchu@gmail.com>
2023-03-13 15:26:40 +0800

    cors

--- src/main/java/com/webank/databrain/config/MvcConfig.java
+++ src/main/java/com/webank/databrain/config/MvcConfig.java
@@ -21,8 +21,8 @@ public class MvcConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins("*")
                 .allowedMethods("*")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+                .allowedHeaders("*");
+//                .allowCredentials(true);
     }
 
     @Override
