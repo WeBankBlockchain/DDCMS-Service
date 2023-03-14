655b428b9799dbe53e9425962e495dfc3905901e
wangyue <wangyue@wangyuedeMac-mini.local>
2023-03-14 11:21:14 +0800

    pro

--- src/main/java/com/webank/databrain/controller/ProductController.java
+++ src/main/java/com/webank/databrain/controller/ProductController.java
@@ -1,10 +1,13 @@
 package com.webank.databrain.controller;
 
-import com.webank.databrain.service.impl.ProductServiceImpl;
+import com.webank.databrain.service.ProductService;
 import com.webank.databrain.vo.common.CommonPageQueryRequest;
 import com.webank.databrain.vo.common.CommonResponse;
 import com.webank.databrain.vo.common.HotDataRequest;
-import com.webank.databrain.vo.request.product.*;
+import com.webank.databrain.vo.request.product.ApproveProductRequest;
+import com.webank.databrain.vo.request.product.CreateProductRequest;
+import com.webank.databrain.vo.request.product.QueryProductByIdRequest;
+import com.webank.databrain.vo.request.product.UpdateProductRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,30 +23,35 @@ import javax.validation.Valid;
 public class ProductController {
 
     @Autowired
-    private ProductServiceImpl productServiceImpl;
+    private ProductService productService;
 
     @PostMapping(value = "/pageQueryProduct")
     public CommonResponse pageQueryProduct(@RequestBody @Valid CommonPageQueryRequest request){
         log.info("pageQueryProduct pageNo = {}, pageSize = {}",request.getPageNo(),request.getPageSize());
-        return productServiceImpl.pageQueryProducts(request);
+        return productService.pageQueryProducts(request);
     }
     @PostMapping(value = "/queryProductById")
     public CommonResponse queryProductById(@RequestBody @Valid QueryProductByIdRequest queryProductRequest) {
         log.info("pageQueryProduct productId = {}", queryProductRequest.getProductGid());
-        return productServiceImpl.getProductDetail(queryProductRequest.getProductGid());
+        return productService.getProductDetail(queryProductRequest.getProductGid());
     }
     @PostMapping(value = "/getHotProducts")
     public CommonResponse getHotProducts(@RequestBody @Valid HotDataRequest request){
-        return productServiceImpl.getHotProducts(request);
+        return productService.getHotProducts(request);
     }
     @PostMapping(value = "/createProduct")
     public CommonResponse createProduct(@RequestBody @Valid CreateProductRequest createProductRequest) throws Exception{
-        return productServiceImpl.createProduct(createProductRequest);
+        return productService.createProduct(createProductRequest);
+    }
+
+    @PostMapping(value = "/approveProduct")
+    public CommonResponse approveProduct(@RequestBody @Valid ApproveProductRequest approveProductRequest) throws Exception{
+        return productService.approveProduct(approveProductRequest);
     }
 
     @PostMapping(value = "/updateProduct")
     public CommonResponse updateProduct(@RequestBody @Valid UpdateProductRequest updateProductRequest) throws Exception{
-        return productServiceImpl.updateProduct(updateProductRequest);
+        return productService.updateProduct(updateProductRequest);
     }
 
 
